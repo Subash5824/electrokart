e556fe4c1441256dd5ce45453cb84aa499317611
Subash <subash5.8.2004@gmail.com>
2026-07-28 11:56:55 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Make sure this points to your running backend
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "https://electrokart-75mh.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Add token to requests if user is logged in
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
