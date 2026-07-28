b711935dfee7603bc4a7638f36da521c70cbe791
Subash <subash5.8.2004@gmail.com>
2026-07-28 19:47:31 +0530

    Add test route to debug backend

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,9 @@ app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
   next();
 });
+app.get("/test", (req, res) => {
+  res.json({ success: true, message: "Backend is reachable!" });
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
