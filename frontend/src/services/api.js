2abcb1bd684319fb9fa963f40fd5a6f7d70da124
Subash <subash5.8.2004@gmail.com>
2026-07-28 14:54:50 +0530

    Fix: Remove /api from backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://electrokart-75mh.onrender.com/api";
+const API_URL = "https://electrokart-75mh.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
